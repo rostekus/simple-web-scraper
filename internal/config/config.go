493859b505f6c3cb7cb52f436f0d9f2e3cf613d5
rostekus <rostyslavmosorov@gmail.com>
2023-10-10 17:43:32 +0200

    feat: add min max word bound

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,8 +17,8 @@ type Config struct {
 type Scraper struct {
 	Timeout time.Duration `yaml:"timeout" env-default:"4s"`
 	MaxGo   uint          `yaml:"maxGo"`
-	MaxLen  uint          `yaml:"maxLen" env-default:"10"`
-	MinLen  uint          `yaml:"minLen" env-default:"1"`
+	MaxLen  int           `yaml:"maxLen" env-default:"10"`
+	MinLen  int           `yaml:"minLen" env-default:"1"`
 }
 
 func MustLoad() *Config {
